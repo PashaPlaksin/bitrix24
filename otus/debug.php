89d3f7951be163ef3987a4b1dc1c60ac2b5fb44d
PashaPlaksin <p.plaksin@gmail.com>
2024-11-21 10:00:29 +0300

    Homework 4. Task 1.

--- otus/debug.php
+++ otus/debug.php
@@ -1,11 +1,14 @@
 <?php
+
+use Bitrix\Main\Diag\Debug as DebugAlias;
+
 require $_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php';
 /*
  * @var Cmain  $APPLICATION
  */
 
 $APPLICATION -> setTitle("Отладка и логирование. ДЗ - 1");
-\Bitrix\Main\Diag\Debug::writeToFile(date("d.m.Y H:i:s"),'',"/logs/1.txt");
+DebugAlias::writeToFile(date("d.m.Y H:i:s"),'',"/logs/1.txt");
 
 echo ("Записали в файл /logs/1.txt текущую дату и время");
 require $_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php';
